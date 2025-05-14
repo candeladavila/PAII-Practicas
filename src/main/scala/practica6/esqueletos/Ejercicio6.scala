5435d2d6645153491166b992cadad925c559d531
candeladavila <candeladavila05@gmail.com>
2025-05-14 10:14:03 +0200

    Ejercicio 4

--- src/main/scala/practica6/esqueletos/Ejercicio6.scala
+++ src/main/scala/practica6/esqueletos/Ejercicio6.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.*
 import scala.util.Random
 
 object mesa {
-  // CS-fumador i: No puede fumar hasta que estén en la mesa los ingredientes que le faltan
+  // CS- fumador i: No puede fumar hasta que estén en la mesa los ingredientes que le faltan
   // CS-Agente: No pone un nuevo ingrediente hasta que el fumador no ha terminado de fumar
   /*
   DESCRIPCIÓN DEL PROBLEMA:
@@ -33,43 +33,33 @@ object mesa {
    */
   private var ingr = -1 // el ingrediente que no está-- -1=mesa vacía, 0=no tabaco, 1=no papel, 2=no cerillas
   private val mutex = new Semaphore (1) //al principio todos pueden poner
-  private val fumando = new Semaphore (0) //al principio no hay nadie fumando
-  private val hayElementos = new Semaphore (1)
+  private val fumando = Array.fill(3)(new Semaphore(0)) //al principio no hay ingredientes y nadie puede fumar
+  private val noHayElementos = new Semaphore (1) //inicialmente no hay elementos
 
   def quieroFumar(i: Int) = {
     // el fumador i quiere fumar
-    hayElementos.acquire()
-    mutex.acquire()
-    if (ingr == i) {
-      ingr = -1 //hemos cogido los elementos para fumar porque era el tipo y entonces ya no queda nada
-      log(s"Fumador $i fuma")
-      fumando.release()
-    } else
-      mutex.release()
-      hayElementos.release()
+    fumando(i).acquire()
+    log(s"Fumador $i fuma")
   }
 
   def finFumar(i: Int) = {
     // el fumador i termina de fumar
-    fumando.acquire()
     log(s"Fumador $i termina de fumar")
-    mutex.release()
+    noHayElementos.release()
   }
 
   def nuevosIngr(ingrediente: Int) = {
     // el agente pone nuevos ingredientes (ingr es el ingrediente que no pone)
+    noHayElementos.acquire() //podrá entrar cuando alguien haya cogido un elemento y terminado de fumar
     mutex.acquire()
-    if (ingr == -1) {
-      log(s"El agente no pone ingrediente $ingrediente")
-      ingr = ingrediente
-      hayElementos.release()
-    }
+    ingr = ingrediente //pone el nuevo ingrediente
+    fumando(ingr).release() //libera al fumador que toque
     mutex.release()
   }
 }
 
 object Ejercicio6 {
-  def main(args: Array[String]): Unit =
+  def main(args: Array[String]): Unit = {
     val fumador = new Array[Thread](3)
     for (i <- fumador.indices)
       fumador(i) = thread {
@@ -84,4 +74,5 @@ object Ejercicio6 {
         Thread.sleep(Random.nextInt(500))
         mesa.nuevosIngr(Random.nextInt(3))
     }
+  }
 }
