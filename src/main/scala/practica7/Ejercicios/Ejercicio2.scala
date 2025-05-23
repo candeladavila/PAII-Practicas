9479e5de8413314c496c18256b99133e723a2d06
candeladavila <candeladavila05@gmail.com>
2025-05-23 14:27:03 +0200

    Ejercicio 2,4,5 práctica 7

--- src/main/scala/practica7/Ejercicios/Ejercicio2.scala
+++ src/main/scala/practica7/Ejercicios/Ejercicio2.scala
@@ -2,6 +2,7 @@ package esqueletosLaboratorio7
 
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
+
 /*
   Ejemplo:
   imaginamos que tenemos un total de 5 recursos
@@ -12,36 +13,72 @@ import scala.util.Random
   La idea es que hay asignar los recursos en el mismo orden en el que se han pedido (First Come First Served)
 
   VARIABLES:
-    -
+    - enEspera = new ListBuffer[Int]() => lista para que almacene los ids de los procesos que están esperando
+    - procEsperando = 0 => variable para controlar el numero de hebras en espera
+    - sigProceso = -1 => variable que tiene el id del siguiente proceso al que le toca
+
   MÉTODOS:
-    - pidoRecursos =>
-    - libRecursos =>
+    - pidoRecursos => primero tiene que mirar si le toca -> tiene que mirar si hay más hebras o no esperando => coge recursos
+    - libRecursos => libra los recursos y despierta al proceso que estaba esperando porque no tenía suficientes recursos disponibles
   NOTAS:
     - La repartición de recursos se hace en orden de solicitud
     - CS-1 = Un proceso se espera cuando alguien ha pedido recursos antes que él y no ha sido atendido (no se llegan a mirar siquiera si hay recursos)
     - CS-2 = Un proceso se tiene que esperar en su turno si no hay suficientes recursos en el sistema para él
+    - Como notify() despierta una hebra aleatoria entonces tenemos que usar una estructura auxiliar para saber a quién tenemos que despertar en cada momento
+      - En el caso de condiciones de sincronización es distinto -> se van bloqueando en orden de llegada (se hará más tarde)
+    - El proceso que esté esperando en la cabeza de la cola tiene una condición de sincronización distinta, espero a que tenga los recursos que necesita
+      - Para despertar a una en concreto hay que despertarla y después que espere en otro sitio diferente a los recursos
+        - Ponemos dos puntos en el código de sincronización diferente para controlarlo
 */
 
 class Recursos(rec:Int) {
 
   
   private var numRec = rec
+  private var enEspera = new ListBuffer[Int]() //inicialmente está vacía porque no hay nadie esperando
+  private var procEsperando = 0
+  private var sigProc = -1  //para almacenar el id del siguiente (se puede omitir si en el if ponemos >= 1 para encolarlos a todos y comprobar la cabeza de la lista
   
-  def pidoRecursos(id:Int,num:Int) =  {
+  def pidoRecursos(id:Int,num:Int) =  synchronized {
     //proceso id solicita num recursos
-      
-      log(s"Proceso $id pide $num recursos.")
-      
-    
-      log(s"Proceso $id coge $num recursos. Quedan $numRec")
+    procEsperando += 1 //me sumo a los procesos que esperan
+    log(s"Proceso $id pide $num recursos.")
+    if (procEsperando > 1){ //si hay más gente esperando tengo que esperar a que me toque
+      enEspera.append(id)
+      while(sigProc !=  id) wait() //CS-1 -> que se ponga a true SOLO si te toca
+    }
+    //Cuando la hebra está aquí es o bien porque ha llegado la primera o no era la primera pero le tocaba
+    while (num > numRec) wait() //CS-2 -> se espera hasta que no tenga los recursos que necesita
+    //Cuando la hebra está aquí entonces ya tiene los recursos para hacer su proceso
+    numRec -= num //coge los recursos que necesita
+    procEsperando -= 1 //ya queda uno menos esperando
+    log(s"Proceso $id coge $num recursos. Quedan $numRec")
+    /*
+    Mira si hay más procesos esperando
+      - si no hay más => FIN
+      - si hay más esperando => DESPIERTA AL SIGUIENTE
+     */
+    if (procEsperando != 0){
+      //tenemos que poner como siguiente proceso al siguiente que haya en la lista de espera
+      if (procEsperando != 0) {
+        sigProc = enEspera.remove(0) //saco el id del siguiente proceso de la estructura y lo guardo en la variable del siguiente proceso
+        notifyAll() //porque no podemos elegir a quien despertamos, después se bloquea
+        //Despertamos a todos, pero como el booleano de while(sigProc!= id) solo lo va a ver cierto uno entonces el resto se van a bloquear
+      } else{
+        //En caso de que no quede nadie esperando tenemos que poner sigProc que esté esperando a -1 => reiniciar la variable para que no se cuele nadie
+        sigProc -= 1
+      }
+    }
+
       
   }
 
-  def libRecursos(id:Int,num:Int) =  {
+  def libRecursos(id:Int,num:Int) =  synchronized {
     //proceso id devuelve num recursos
-   
+    numRec += num //aumentamos los recursos disponibles al liberarlo
     log(s"Proceso $id devuelve $num recursos. Quedan $numRec")
-    
+    //La intención es despertar al que estaba esperando porque no tenía suficientes recursos libres
+    notifyAll() //despertamos a todos porque no podemos elegir a quien despertar
   }
 }
 object Ejercicio2 {
--- src/main/scala/practica7/Ejercicios/Ejercicio5.scala
+++ src/main/scala/practica7/Ejercicios/Ejercicio5.scala
@@ -1,36 +1,120 @@
 package esqueletosLaboratorio7
 
 import scala.util.Random
+/*
+  Descripción el problema:
+    Hay dos tipos de estudiantes: Iphone y Android
+    La barca tiene una capacidad 4 (4 asientos) y siempre se tiene que garantizar que el número de cada tipo tiene que ser balanceado
+    Configuraciones posibles:
+      - 4 Android
+      - 4 iPhone
+      - 2 Android y 2 iPhone
 
+  VARIABLES:
+    - var nIphone
+    - var nAnd
+    - var puertaSalidaAbierta => con una es suficiente porque todos tienen que bajar
+    - var puertaEntradaAndroid => para controlar si puede subir o no un android (CS-And)
+    - var puertaEntradaIphone => para controlar si puede subir o no un iPhone (CS-Iph)
+
+  MÉTODOS:
+    - paseoIphone
+    - paseoAndroid
+
+  NOTAS:
+    - Condiciones de sincronización:
+        - CS-And = espero si está lleno o si la configuración no es segura (se provoca)
+        - CS-iPh = espero si está lleno o si la configuración no es segura (se provoca)
+        - CS-Todos = espero hasta el final del paseo (Muy parecida a la del coche -> ponemos una puerta de salida)
+*/
 object Barca{
+  //Contadores de estudiantes de cada tipo
   private var nIPhone = 0
   private var nAndroid = 0
-  
+  private var puertaSalidaAbierta = false
+  private var puertaEntradaAndroid = true
+  private var puertaEntradaIphone = true
 
-  def paseoIphone(id:Int) =  {
-   
+  def paseoIphone(id: Int) = synchronized {
+    while (!puertaEntradaIphone) wait() //Si no está abierta su puerta no entra
+    //Cuando la hebra está aquí es porque el iphone ya está dentro
+    nIPhone += 1
     log(s"Estudiante IPhone $id se sube a la barca. Hay: iphone=$nIPhone,android=$nAndroid ")
-    
-    
-      //log(s"Empieza el viaje....")
-      //Thread.sleep(Random.nextInt(200))
-      //log(s"fin del viaje....")
-      
-    
+    //Ahora tenemos que mirar cuando cerrar qué puertas (analizar el número de cada tipo)
+    /*
+    Si cuando entro la suma de los dos tipos da 3 entonces es crítico y ya tenemos que analizar que puerta abrir
+     */
+    if (nIPhone + nAndroid == 3) { //he sido el tercero en entrar y tengo que analizar la situación actual
+      if (nIPhone == 3 || nIPhone == 1) { //no puede entrar ningún android
+        puertaEntradaAndroid = false
+      } else { //hay un android tengo que cerrar la puerta a los iphone
+        puertaEntradaIphone = false
+      }
+    }
+    if (nIPhone + nAndroid < 4) { //si no soy el último en subirme entonces me espero
+      while (!puertaSalidaAbierta) wait()
+    } else { //soy el último en entrar
+      //Cierro las dos puertas de entrada
+      puertaEntradaAndroid = false
+      puertaEntradaIphone = false
+      log(s"Empieza el viaje....")
+      Thread.sleep(Random.nextInt(200))
+      log(s"fin del viaje....")
+      //Abro la puerta de salida y notifico a todas las hebras
+      puertaSalidaAbierta = true
+      notifyAll()
+    }
+    //Cuando la hebra está aquí es porque le toca bajarse
+    nIPhone -= 1
     log(s"Estudiante IPhone $id se baja de la barca. Hay: iphone=$nIPhone,android=$nAndroid ")
-   
+    if (nIPhone + nAndroid == 0) { //si soy el último en bajarme
+      puertaSalidaAbierta = false //cierro la puerta de salida
+      //Abro las dos puertas de entrada
+      puertaEntradaIphone = true
+      puertaEntradaAndroid = true
+      log (s"**********************************************")
+      notifyAll() //despierto a todas las hebras
+    }
   }
 
-  def paseoAndroid(id:Int) =  {
-    
+  def paseoAndroid(id:Int) =  synchronized {
+    while(!puertaEntradaAndroid) wait()
+    nAndroid += 1
     log(s"Estudiante Android $id se sube a la barca. Hay: iphone=$nIPhone,android=$nAndroid ")
-    
-      //log(s"Empieza el viaje....")
-      //Thread.sleep(Random.nextInt(200))
-      //log(s"fin del viaje....")
-      
+    /*
+    Si cuando entro la suma de los dos tipos da 3 entonces es crítico y ya tenemos que analizar que puerta abrir
+     */
+    if (nIPhone + nAndroid == 3){
+      if (nAndroid == 3 || nAndroid== 1){
+        puertaEntradaIphone = false
+      } else {
+        puertaEntradaAndroid = false
+      }
+    }
+    if (nIPhone + nAndroid < 4){ //si no soy el último en subirme entonces me espero
+      while (!puertaSalidaAbierta) wait()
+    } else{ //soy el último en entrar
+      //Cierro las dos puertas de entrada
+      puertaEntradaAndroid = false
+      puertaEntradaIphone = false
+      log(s"Empieza el viaje....")
+      Thread.sleep(Random.nextInt(200))
+      log(s"fin del viaje....")
+      //Abro la puerta de salida y notifico a todas las hebras
+      puertaSalidaAbierta = true
+      notifyAll()
+    }
+    //Cuando la hebra está aquí es porque le toca bajarse
+    nAndroid -= 1
     log(s"Estudiante Android $id se baja de la barca. Hay: iphone=$nIPhone,android=$nAndroid ")
-    
+    if (nIPhone + nAndroid == 0){ //si soy el último en bajarme
+      puertaSalidaAbierta = false //cierro la puerta de salida
+      //Abro las dos puertas de entrada
+      puertaEntradaIphone = true
+      puertaEntradaAndroid = true
+      log (s"**********************************************")
+      notifyAll() //despierto a todas las hebras
+    }
   }
 }
 object Ejercicio5 {
